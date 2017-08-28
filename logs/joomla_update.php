db3e255216263bc33404e855969b37577b784966
Lewis Jang <lewis.aedes@gmail.com>
2017-08-28 02:28:57 +0000

    Backup

---
+++ logs/joomla_update.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-08-28 02:19:59 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-08-28T02:19:59+00:00	INFO 36.234.169.49	update	更新由使用者 AEdes (584) 開始進行. 舊版本是 3.4.8.
+2017-08-28T02:19:59+00:00	INFO 36.234.169.49	update	從 https://downloads.joomla.org/cms/joomla3/3-7-5/Joomla_3.7.5-Stable-Update_Package.zip 下載更新檔案.
+2017-08-28T02:21:00+00:00	INFO 36.234.169.49	update	檔案 Joomla_3.7.5-Stable-Update_Package.zip 已成功下載.
